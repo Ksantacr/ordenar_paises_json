fee396c5f583cb3ab155527be8b12f29a09fed0b
Ksantacr <defonix0@gmail.com>
2017-06-08 13:36:27 -0500

    base

---
+++ js/script.js
@@ -0,0 +1,27 @@
+var json = '[{ "Name" : "Alfreds Futterkiste", "City" : "Berlin", "Country" : "Germany" }, { "Name" : "Berglunds snabbköp", "City" : "Luleå", "Country" : "Sweden" }, { "Name" : "Centro comercial Moctezuma", "City" : "México D.F.", "Country" : "Mexico" }, { "Name" : "Ernst Handel", "City" : "Graz", "Country" : "Austria" }, { "Name" : "FISSA Fabrica Inter. Salchichas S.A.", "City" : "Madrid", "Country" : "Spain" }, { "Name" : "Galería del gastrónomo", "City" : "Barcelona", "Country" : "Spain" }, { "Name" : "Island Trading", "City" : "Cowes", "Country" : "UK" }, { "Name" : "Königlich Essen", "City" : "Brandenburg", "Country" : "Germany" }, { "Name" : "Laughing Bacchus Wine Cellars", "City" : "Vancouver", "Country" : "Canada" }, { "Name" : "Magazzini Alimentari Riuniti", "City" : "Bergamo", "Country" : "Italy" }, { "Name" : "North/South", "City" : "London", "Country" : "UK" }, { "Name" : "Paris spécialités", "City" : "Paris", "Country" : "France" }, { "Name" : "Rattlesnake Canyon Grocery", "City" : "Albuquerque", "Country" : "USA" }, { "Name" : "Simons bistro", "City" : "København", "Country" : "Denmark" }, { "Name" : "The Big Cheese", "City" : "Portland", "Country" : "USA" }, { "Name" : "Vaffeljernet", "City" : "Århus", "Country" : "Denmark" }, { "Name" : "Wolski Zajazd", "City" : "Warszawa", "Country" : "Poland" }]';
+
+var obj = JSON.parse(json);
+
+var paises = [];
+obj.forEach(function(element) {
+  
+  for(var prop in element){
+
+   if(paises.indexOf(element[prop])==-1){
+      paises.push(element[prop]); 
+   }
+
+  }
+}, this);
+
+var tablaPaises = document.getElementById("paises");
+paises = paises.sort();
+paises.forEach(function(pais){
+
+  var templatePais = "<td>pais</td>";
+  templatePais = templatePais.replace("pais", pais);
+  var tr = document.createElement("tr");
+  tr.innerHTML = "<tr>"+templatePais+"</tr>"
+  tablaPaises.appendChild(tr);
+
+});
